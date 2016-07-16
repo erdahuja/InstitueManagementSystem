1a26401e1470adadde1082e9bdeb2880ed5e6cf2
Deepak <er.d.ahuja@gmail.com>
2016-07-16 16:14:10 +0530

    Add files via upload

--- IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
+++ IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
@@ -5,6 +5,7 @@ public static String LOGIN_SQL = "select userid,password from user_mst where use
 public static String ADDCOURSE_SQL="INSERT INTO `InstituteProject`.`courses_table`(`course_name`,`fees`,`course_description`,`duration`,`trainer_name`) VALUES(?,?,?,?,?)";
 public static String VIEWCOURSE_SQL="SELECT `course_name` FROM `InstituteProject`.`courses_table`";
 
+public static String DELETECOURSE_SQL="DELETE FROM `InstituteProject`.`courses_table` WHERE course_name=?";
 
 
 
