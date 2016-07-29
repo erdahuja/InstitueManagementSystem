92c87bc85bf63370801dede646fb56b89a18cb76
ojas7160 <ojas.vwadhwa1995@gmail.com>
2016-07-29 16:59:35 +0530

    Add files via upload

--- IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
+++ IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
@@ -1,13 +1,16 @@
-package com.bmpl.ims.common.dao;
+
+package com.bmpl.ojas.Common;
 
 public interface CommonSQLConstants {
-	public static String LOGIN_SQL = "select userid,password from user_mst where userid=? and password=?";
-	public static String ADDCOURSE_SQL = "INSERT INTO `InstituteProject`.`courses_table`(`course_name`,`fees`,`course_description`,`duration`,`trainer_name`) VALUES(?,?,?,?,?)";
-	public static String VIEWCOURSE_SQL = "SELECT `course_name` FROM `InstituteProject`.`courses_table`";
-	public static String CHOOSECOURSE_SQL = "SELECT * FROM `InstituteProject`.`courses_table` where course_name=? ";
+ 
+public static String REGISTRATION_SQL = 
+"INSERT INTO `registerdb`.`register_page`(`Name`,`Address`,`Country`,`State`,`Phone`,`Email"
++ "`,`Fname`) VALUES(?,?,?,?,?,?,?)";
 
-	public static String DELETECOURSE_SQL = "DELETE FROM `InstituteProject`.`courses_table` WHERE course_name=?";
-
-	public static String UPDATECOURSE_SQL = "UPDATE `InstituteProject`.`courses_table` SET 'course_name` = ?,`fees` = ?,`course_description` = ?,`duration` = ?,`trainer_name` = ? WHERE `course_name` = ?";
-
+public static String VIEWCOURSE_SQL="SELECT `name` FROM `registerdb`.`register_page`";
+public static String ADDTUTOR_SQL="INSERT INTO `registerdb`.`register_page`.`courses_table`(`course_name`,`fees`,`course_description`,`duration`,`trainer_name`) VALUES(?,?,?,?,?)";
+public static String DELETECOURSE_SQL="DELETE FROM `registerdb`.`register_page` WHERE `name`=?";
+public static String UPDATECOURSE_SQL="UPDATE `registerdb`.`register_page` SET `name`=?";
 }
+
+
