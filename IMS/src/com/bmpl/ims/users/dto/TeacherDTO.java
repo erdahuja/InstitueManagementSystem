24a8e64a748a761c96eae7c1a3aedd4a6d7b3c6b
ojas7160 <ojas.vwadhwa1995@gmail.com>
2016-07-29 16:56:42 +0530

    Respective DTOs

---
+++ IMS/src/com/bmpl/ims/users/dto/TeacherDTO.java
@@ -0,0 +1,41 @@
+package com.bmpl.ojas.DTO;
+
+public class TeacherDTO {
+	
+	private String Name ;
+	String course_name;
+	String course_description;
+
+	public String getCourse_name() {
+		return course_name;
+	}
+
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+
+	public String getCourse_description() {
+		return course_description;
+	}
+
+	public void setCourse_description(String course_description) {
+		this.course_description = course_description;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "TeacherDTO [Name=" + Name + ", course_name=" + course_name + ", course_description="
+				+ course_description + "]";
+	}
+
+	
+}
+
