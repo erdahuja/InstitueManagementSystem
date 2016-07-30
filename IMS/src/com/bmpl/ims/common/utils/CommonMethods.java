341cfd6599d80c7c3bf15b5d2ac0d728927da68a
Deepak <er.d.ahuja@gmail.com>
2016-07-30 22:36:09 +0530

    Add files via upload

--- IMS/src/com/bmpl/ims/common/utils/CommonMethods.java
+++ IMS/src/com/bmpl/ims/common/utils/CommonMethods.java
@@ -6,14 +6,66 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
-
 import com.bmpl.ims.common.dao.CommonDAO;
 import com.bmpl.ims.common.dao.CommonSQLConstants;
-import com.bmpl.ims.users.dao.CoursesDAO;
 
 public class CommonMethods {
 	
+	
+public static ArrayList<String> getStudents() throws SQLException {
+		
+		String sql = CommonSQLConstants.VIEWSTUDENTS_SQL;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs;
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			con = CommonDAO.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				list.add(rs.getString("studentsName"));//change acc to db.!!
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+
+		} finally {
+			con.close();
+		}
+		return list;
+
+	}	
+	public static ArrayList<String> getBatches() throws SQLException {
+		
+		String sql = CommonSQLConstants.VIEWBATCHES_SQL;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs;
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			con = CommonDAO.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				list.add(rs.getString("batch_name"));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+
+		} finally {
+			con.close();
+		}
+		return list;
+
+	}	
 	
 	public static ArrayList<String> getCourse() throws SQLException {
 		boolean coursesShown = false;
