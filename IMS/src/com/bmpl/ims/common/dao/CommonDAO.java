4895025016169ece932ace206912db5c97e37756
Sunil <vishkarmasunil@gmail.com>
2016-08-01 21:58:07 +0530

    CommonSQLConstants Uploaded

--- IMS/src/com/bmpl/ims/common/dao/CommonDAO.java
+++ IMS/src/com/bmpl/ims/common/dao/CommonDAO.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class CommonDAO {
-	private CommonDAO() {
-	}
-
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		ResourceBundle rb = ResourceBundle.getBundle("db");
+	private CommonDAO(){}
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+		//String url2 = "jdbc:mysql://localhost:3306/admin?user=root&password=sunil2345";
+		ResourceBundle rb=ResourceBundle.getBundle("db");
 		Class.forName(rb.getString("drivername"));
-		Connection con = DriverManager.getConnection(rb.getString("dburl"), rb.getString("userid"),
-				rb.getString("password"));
+		//Class.forName("com.mysql.jdbc.Driver");
+		Connection con= DriverManager.getConnection(rb.getString("dburl"),rb.getString("userid"),rb.getString("password"));
+		//Connection con=DriverManager.getConnection(url2);
 		return con;
 	}
-
+	
 }
--- IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
+++ IMS/src/com/bmpl/ims/common/dao/CommonSQLConstants.java
@@ -1,20 +1,18 @@
 package com.bmpl.ims.common.dao;
 
 public interface CommonSQLConstants {
-	public static String LOGIN_SQL = "select userid,password from user_mst where userid=? and password=?";
-	public static String ADDCOURSE_SQL = "INSERT INTO `InstituteProject`.`courses_table`(`course_name`,`fees`,`course_description`,`duration`,`trainer_name`) VALUES(?,?,?,?,?)";
-	public static String VIEWCOURSE_SQL = "SELECT `course_name` FROM `InstituteProject`.`courses_table`";
-	public static String CHOOSECOURSE_SQL = "SELECT * FROM `InstituteProject`.`courses_table` where course_name=? ";
+//public static String LOGIN_SQL = "select userid,password from user_mst where userid=? and password=?";
+public static String LOGIN_SQL = "select user_master.`userid`,"
+		+ "role_master.`rolename`,right_master.`rightname`, "
+		+" `right_master`.`screenname`,right_master.`category` "
++" from user_master , role_master, right_master , user_role_mapping, "
++" role_right_mapping where user_master.`userid`=? "
++" and user_master.`pwd`=? and user_master.`uid`=user_role_mapping.`uid` "
++" and role_master.`roleid`=user_role_mapping.`roleid` "
++" and role_master.`roleid`=role_right_mapping.`roleid` and "
++ " right_master.`rightid`=role_right_mapping.`rightid`";
 
-	public static String DELETECOURSE_SQL = "DELETE FROM `InstituteProject`.`courses_table` WHERE course_name=?";
-
-	public static String UPDATECOURSE_SQL = "UPDATE `InstituteProject`.`courses_table` SET 'course_name` = ?,`fees` = ?,`course_description` = ?,`duration` = ?,`trainer_name` = ? WHERE `course_name` = ?";
-	public static String VIEWBATCHES_SQL = "";
-	public static String VIEWSTUDENTS_SQL="";
-	public static String TAKEATTENDANCE_SQL = "";
-	public static String ENQUIRE_SQL = "INSERT INTO `project`.`enquire_sql`"
-			+ "(`firstName`,`secondName`,`address1`,`address2`,`email`,`mobileContact`,`otherContact`,`slot1`,`slot2`,`fatherFirstName`,"
-			+ "`fatherSecondName`,`fatherMobileContact`,`fatherOtherContact`,`university`,`qualification`,`reference`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	public static String COURSE_SQL="SELECT * from `project`.`course_sql`";
-	
+public static String ADDBATCH_SQL="Insert into batch_table (batchname, coursename, trainername, totalsets, description, batch_starts) Values (?,?,?,?,?,?)";
+public static String LOGIN_SQL2="select username,password from user_table where username=? and password=?";
+public static String GETCOURSE_SQL="select * from course_table";
 }
