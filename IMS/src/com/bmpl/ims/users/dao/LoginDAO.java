d21056a9870d250669e0d89c503f491706b441e0
Sunil <vishkarmasunil@gmail.com>
2016-08-04 16:47:08 +0530

    LoginDao

--- IMS/src/com/bmpl/ims/users/dao/LoginDAO.java
+++ IMS/src/com/bmpl/ims/users/dao/LoginDAO.java
@@ -11,9 +11,39 @@ import com.bmpl.ims.common.dao.CommonSQLConstants;
 import com.bmpl.ims.users.dto.UserDTO;
 
 public class LoginDAO {
-	public boolean authenticateUser(UserDTO userDTO) throws ClassNotFoundException, SQLException{
+	public boolean authenticateAdmin(UserDTO userDTO) throws ClassNotFoundException, SQLException{
 		boolean isFound = false;
-		String sql  = CommonSQLConstants.LOGIN_SQL2;
+		String sql  = CommonSQLConstants.LOGIN_SQLAdmin;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		Statement stmt=null;
+		ResultSet rs = null;
+		try{
+			con = CommonDAO.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userDTO.getUserid());
+			pstmt.setString(2, userDTO.getPassword());
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				isFound = true;
+			}
+		}
+		finally{
+			if(rs!=null){
+				rs.close();
+			}
+			if(pstmt!=null){
+				pstmt.close();
+			}
+			if(con!=null){
+				con.close();
+			}
+		}
+		return isFound;
+	}
+	public boolean authenticateCounselor(UserDTO userDTO) throws ClassNotFoundException, SQLException{
+		boolean isFound = false;
+		String sql  = CommonSQLConstants.LOGIN_SQLCounselor;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		Statement stmt=null;
