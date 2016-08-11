9166ce33fb6896c5ed4a32cb3924f7733a1b83fa
Sunil <vishkarmasunil@gmail.com>
2016-08-11 14:28:21 +0530

    updated registerdao

--- IMS/src/com/bmpl/ims/users/dao/RegisterDAO.java
+++ IMS/src/com/bmpl/ims/users/dao/RegisterDAO.java
@@ -1,47 +1,56 @@
 package com.bmpl.ims.users.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.bmpl.ims.common.dao.CommonDAO;
 import com.bmpl.ims.common.dao.CommonSQLConstants;
-import com.bmpl.ims.users.dto.TrainerDTO;
+import com.bmpl.ims.users.dto.RegisterDTO;
 
-public interface TrainerDao {
+public class RegisterDAO {
 
-	public static  boolean insertData(TrainerDTO trainer) throws ClassNotFoundException, SQLException{
-		boolean istrue = false;
-		String sql  = CommonSQLConstants.Trainer_Registration_SQl;
+	public boolean register(RegisterDTO registerDTO) throws SQLException, ClassNotFoundException {
+
+		boolean registration = false;
+		String sql = com.bmpl.ims.common.dao.CommonSQLConstants.REGISTRATION_SQL;
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		con = CommonDAO.getConnection();
-		
-		pstmt = con.prepareStatement(sql);
-		pstmt.setString(1, trainer.getTrainer_Name());
-		pstmt.setInt(2, trainer.getTrainer_Age());
-		pstmt.setString(3, trainer.getTrainer_Gender());
-		pstmt.setString(4, trainer.getTrainer_Mobileno());
-		pstmt.setString(5, trainer.getTrainer_Address());
-		pstmt.setString(6, trainer.getTrainer_Email());
-		pstmt.setString(7, trainer.getTrainer_Subject());
-		pstmt.setString(8, trainer.getTrainer_Image());
-		pstmt.setDate(9, (Date)trainer.getTrainer_Hire_Date());
-		int result = pstmt.executeUpdate();
-		
-		if(result > 0 ){
-			System.out.println("data insert succesfully");
-			istrue = true;
+		int rs;
+
+		try {
+			con = CommonDAO.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, registerDTO.getName());
+			pstmt.setString(2, registerDTO.getFname());
+			pstmt.setString(3, registerDTO.getAddress());
+			pstmt.setString(4, registerDTO.getPhone());
+			/*
+			 * pstmt.setString(5, registerDTO.getBatch()); 
+			 * pstmt.setString(6, registerDTO.getCountry());
+			 * 
+			 */
+			pstmt.setString(5, registerDTO.getMail());
+			pstmt.setString(6, registerDTO.getState());
+			pstmt.setString(7, registerDTO.getCourse());
+			rs = pstmt.executeUpdate();
+
+			try {
+
+				registration = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Registration Failed");
+			}
+
+		} finally {
+			if (con != null) {
+				con.close();
+			}
 		}
-		con.close();
-		pstmt.close();
-		return istrue;
-		
-		
-		
-		
-		
+
+		return registration;
+
 	}
-	
+
 }
