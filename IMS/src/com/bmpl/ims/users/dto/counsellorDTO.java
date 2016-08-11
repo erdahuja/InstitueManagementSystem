9084dc8abea608853c75c5bde3005d2ea33cf248
Sunil <vishkarmasunil@gmail.com>
2016-08-11 15:36:08 +0530

    counsellorDTO uploaded

---
+++ IMS/src/com/bmpl/ims/users/dto/counsellorDTO.java
@@ -0,0 +1,34 @@
+package com.bmpl.counsellorform.DTO;
+
+public class counsellorDTO {
+	private String name;
+	private String address;
+	private String phonenumber;
+	private String qualification;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+}
+	
+
