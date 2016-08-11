371c5480aebff6f0381a6aadcd82ad74cfe57e57
Himanshu sharma <himanshu648594@gmail.com>
2016-08-11 13:25:49 +0530

    updated trainer form

--- IMS/src/com/bmpl/ims/users/views/TrainerRegistration.java
+++ IMS/src/com/bmpl/ims/users/views/TrainerRegistration.java
@@ -8,13 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Date;
 
-import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -23,7 +20,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.Timer;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.BevelBorder;
@@ -32,35 +28,42 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import com.bmpl.ims.users.dao.TrainerDao;
 import com.bmpl.ims.users.dto.TrainerDTO;
 
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+
 public class TrainerRegistration extends JFrame {
-	private JTextField txtname;
-	private JTextField txtage;
-	private JTextField txtmobile;
-	private JTextField txtemail;
-	private JTextField txtsubject;
-	private JLabel lblImg;
+	private static JTextField txtname;
+	private static JTextField txtage;
+	private static JTextField txtmobile;
+	private static JTextField txtemail;
+	
+	private static JTextField txtsubject;
 	private String path;
 	private Choice  choicegender;
 	private static int counter = 0;
 	private ImageIcon icon = new ImageIcon(TrainerRegistration.this.getClass().getResource("green-check.png"));
-	private JTextArea txtaddress;
+	private static JTextArea txtaddress;
 	
 	private ImageIcon icon1 = new ImageIcon(TrainerRegistration.this.getClass().getResource("red-cross.png"));
-	private JLabel l1;
-	private JLabel l2;
-	private JLabel l3;
-	private JLabel l4;
-	private JLabel l5;
-	private JLabel l6;
-	private JLabel l7;
-	private JLabel l8;
-	
+	private static JLabel l1;
+	private static JLabel l2;
+	private static JLabel l3;
+	private static JLabel l4;
+	private static JLabel l5;
+	private static JLabel l6;
+	private static JLabel l7;
+	private static JLabel l8;
+	private static TrainerRegistration frame;
+	private static JLabel lblImage;
+	private 	JDatePickerImpl datePicker;
 
 	
 	
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		
-					TrainerRegistration frame = new TrainerRegistration();
+					frame = new TrainerRegistration();
 					frame.setVisible(true);
 					
 	}
@@ -91,14 +94,14 @@ public class TrainerRegistration extends JFrame {
 		getContentPane().add(lblAge);
 		
 		 choicegender = new Choice();
-		 choicegender.setFont(null);
+		 choicegender.setFont(new Font("Dialog", Font.BOLD, 15));
+		
 		 choicegender.setForeground(Color.BLACK);
 		 choicegender.setBackground(Color.WHITE);
 		 choicegender.add("--select--");
 		 choicegender.add("Male");
 		 choicegender.add("Female");
-		choicegender.setIgnoreRepaint(true);
-		choicegender.setBounds(178, 158, 194, 21);
+		 choicegender.setBounds(178, 158, 194, 21);
 		getContentPane().add(choicegender);
 		
 		JLabel lblGender = new JLabel("Gender");
@@ -140,7 +143,7 @@ public class TrainerRegistration extends JFrame {
 		txtmobile.setColumns(10);
 		
 		txtaddress = new JTextArea();
-		txtaddress.setForeground(Color.WHITE);
+		txtaddress.setForeground(Color.BLACK);
 		txtaddress.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.DARK_GRAY, Color.DARK_GRAY, Color.DARK_GRAY));
 		txtaddress.setBounds(178, 185, 194, 70);
 		getContentPane().add(txtaddress);
@@ -157,19 +160,6 @@ public class TrainerRegistration extends JFrame {
 		getContentPane().add(txtsubject);
 		txtsubject.setColumns(10);
 		
-		lblImg = new JLabel("");
-		lblImg.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				getImage();
-			}
-		});
-		lblImg.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.DARK_GRAY, Color.DARK_GRAY, Color.DARK_GRAY));
-		lblImg.setToolTipText("Trainer Image");
-		lblImg.setBounds(409, 57, 149, 160);
-		getContentPane().add(lblImg);
-		
 		JLabel lblimg = new JLabel("");
 		lblimg.setBounds(435, 95, 70, 15);
 		
@@ -178,7 +168,7 @@ public class TrainerRegistration extends JFrame {
 		
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.setBackground(new Color(30, 144, 255));
-		btnSubmit.setBounds(429, 293, 117, 40);
+		btnSubmit.setBounds(424, 360, 117, 40);
 		btnSubmit.addActionListener(new ActionListener()
 		{
 		  public void actionPerformed(ActionEvent e){
@@ -222,8 +212,57 @@ public class TrainerRegistration extends JFrame {
 		getContentPane().add(l7);
 		
 		l8 = new JLabel("");
-		l8.setBounds(471, 251, 34, 27);
+		l8.setBounds(458, 251, 34, 27);
 		getContentPane().add(l8);
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			clear();
+				
+			}
+		});
+		btnReset.setBackground(new Color(30, 144, 255));
+		btnReset.setBounds(424, 290, 117, 40);
+		getContentPane().add(btnReset);
+
+		
+		JButton btnViewTeachers = new JButton("View Teachers");
+		
+		btnViewTeachers.setBackground(new Color(30, 144, 255));
+		btnViewTeachers.setBounds(396, 423, 162, 40);
+		getContentPane().add(btnViewTeachers);
+		
+		JLabel lblHiredate = new JLabel("HireDate");
+		lblHiredate.setBounds(35, 436, 70, 15);
+		getContentPane().add(lblHiredate);
+		
+		UtilDateModel model = new UtilDateModel();
+		
+		JDatePanelImpl datePanel = new JDatePanelImpl(model);
+	datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+		 datePicker.setBounds(176, 423, 196, 25);
+		getContentPane().add(datePicker);
+		
+		lblImage = new JLabel("");
+		
+		lblImage.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+					getImage();
+				
+			}
+		});
+		lblImage.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 0), new Color(0, 0, 0), new Color(0, 0, 0), new Color(0, 0, 0)));
+		lblImage.setBounds(409, 57, 139, 145);
+		getContentPane().add(lblImage);
+		
+		
+		
 		
 		
 	
@@ -231,37 +270,37 @@ public class TrainerRegistration extends JFrame {
 	
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 	}
-	   public ImageIcon ResizeImage(String ImagePath)
-	    {
-	        ImageIcon MyImage = new ImageIcon(ImagePath);
-	        Image img = MyImage.getImage();
-	        Image newImg = img.getScaledInstance(lblImg.getWidth(), lblImg.getHeight(), Image.SCALE_SMOOTH);
-	        ImageIcon image = new ImageIcon(newImg);
-	        return image;
-	    }
-	 
-	private void getImage(){
+	public ImageIcon ResizeImage(String path)
+    {
+	ImageIcon ic = new ImageIcon(path);
+     
+        Image img = ic.getImage();
+        Image newImg = img.getScaledInstance(lblImage.getWidth(),lblImage.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(newImg);
+        return image;
+    }
+ 
+	private void getImage() {
 		 	JFileChooser fc = new JFileChooser();
 		 	fc.setDialogTitle("Choose Your Image");
 		    fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		    fc.setCurrentDirectory(new File(System.getProperty("user.home")));
-		    final FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, GIF, & PNG Images", "jpg", "gif", "png");
+		    final FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, GIF, & PNG Images", "jpg", "gif", "png","jpeg");
 		    fc.setFileFilter(filter);
 		    int returnval=fc.showOpenDialog(this);
+		   
 		    if(returnval==JFileChooser.APPROVE_OPTION)
 		    {
-		        File file = fc.getSelectedFile();
+		        fc.getSelectedFile();
 		        path = fc.getSelectedFile().getPath();
-		        BufferedImage bi;
-		        try
-		        {   
-		            bi=ImageIO.read(file);
-		            lblImg.setIcon(new ImageIcon(bi));
-		        }
-		        catch(IOException e)
-		        {
-		        	JOptionPane.showMessageDialog(this, "Error while set image"+e,"error", JOptionPane.ERROR_MESSAGE);
-		        }
+		      
+		    
+		    lblImage.setIcon((ImageIcon) ResizeImage(path));
+		    if(lblImage!=null){
+		    	JOptionPane.showMessageDialog(this, "image found");
+		    }
+		           
+		      
 
 		        }   else if(returnval == JFileChooser.CANCEL_OPTION){
 		        	JOptionPane.showMessageDialog(this, "No selected image"+"error");
@@ -269,24 +308,24 @@ public class TrainerRegistration extends JFrame {
 	}
 	
 	private void getData() throws ClassNotFoundException, SQLException{
-
+		
 		TrainerDTO trainer = new TrainerDTO();
 		if(txtname.getText().trim().length()!=0){
-			trainer.setTrainer_name(txtname.getText());
+			trainer.setTrainer_Name(txtname.getText());;
 			counter++;
 			l1.setIcon(icon);
 		}else{
 			l1.setIcon(icon1);
 		}
 		if(txtage.getText().trim().length()<3 && txtage.getText().trim().length() !=0){
-			trainer.setTrainer_age(Integer.parseInt(txtage.getText()));
+			trainer.setTrainer_Age(Integer.parseInt(txtage.getText()));
 			l2.setIcon(icon);
 			counter++;
 			}else{
 				l2.setIcon(icon1);
 			}
 		if(choicegender.getSelectedItem() == "Male" ||choicegender.getSelectedItem() == "Female"){
-			trainer.setTrainer_gender(choicegender.getSelectedItem());
+			trainer.setTrainer_Gender(choicegender.getSelectedItem());
 			l3.setIcon(icon);
 			counter++;
 		}else{
@@ -294,7 +333,7 @@ public class TrainerRegistration extends JFrame {
 			counter++;
 		}
 		if(txtaddress.getText().trim().length()!=0 && txtaddress.getText().trim().length() >10){
-			trainer.setTrainer_address(txtaddress.getText());
+			trainer.setTrainer_Address(txtaddress.getText());
 			l4.setIcon(icon);
 			counter++;
 		}else{
@@ -302,7 +341,7 @@ public class TrainerRegistration extends JFrame {
 			
 		}
 		if(txtmobile.getText().trim().length()==10){
-			trainer.setTrainer_mobileno(txtmobile.getText());
+			trainer.setTrainer_Mobileno(txtmobile.getText());
 			l5.setIcon(icon);
 			counter++;
 		}else{
@@ -311,7 +350,7 @@ public class TrainerRegistration extends JFrame {
 		if(txtemail.getText().trim().length()!=0){
 			if(txtemail.getText().trim().endsWith("@gmail.com") || txtemail.getText().trim().endsWith("@yahoo.com")){
 				counter++;
-				trainer.setTrainer_email(txtemail.getText());
+				trainer.setTrainer_Email(txtemail.getText());
 				l6.setIcon(icon);
 			}else{
 				l6.setIcon(icon1);
@@ -321,29 +360,60 @@ public class TrainerRegistration extends JFrame {
 		}
 			if(txtsubject.getText().trim().length()!=0){
 				counter++;
-				trainer.setTrainer_subject(txtsubject.getText());
+				trainer.setTrainer_Subject(txtsubject.getText());
 				l7.setIcon(icon);
 			}else{
 				l7.setIcon(icon1);
 			}
-		if(lblImg.getIcon()!=null){
+		if(lblImage.getIcon()!=null){
 			counter++;
-			trainer.setTrainer_image(path);
+			trainer.setTrainer_Image(path);
 			l8.setIcon(icon);
 		}else{
 			l8.setIcon(icon1);
 		}
-		if(counter == 8){
+	
+		if(counter != 8){
+			System.out.println("data not saved");
+		
+			
+			
+		}else{
+			counter = 0;
+			Date selectedDate = (Date) datePicker.getModel().getValue();
+			java.sql.Date sqlDate = new java.sql.Date(selectedDate.getTime());
+		
+			trainer.setTrainer_Hire_Date(sqlDate);
 			System.out.println(trainer);
-			boolean istrue = TrainerDao.insertData(trainer);
+		boolean istrue = 	TrainerDao.insertData(trainer);
 			if(istrue){
 				JOptionPane.showMessageDialog(this, "Data Saved Succesfully");
 			}else{
 				JOptionPane.showMessageDialog(this, "Data not Saved");
 			}
-			
-			
 		}
 	}
-
+	private static void clear(){
+		  l1.setIcon(null); 
+  		  l2.setIcon(null);
+  		  l3.setIcon(null);
+  		  l4.setIcon(null);
+  		  l5.setIcon(null);
+  		  l6.setIcon(null);
+  		  l7.setIcon(null);
+  		  l8.setIcon(null);
+  		  lblImage.setIcon(null);
+  		  txtname.setText("");;
+  		  txtage.setText("");
+  		  txtmobile.setText("");
+  		  txtemail.setText("");
+  		  txtsubject.setText("");
+  		  txtaddress.setText("");
+  		  counter = 0;
+  		  
+		    
+		  
+	}
 }
+
+
