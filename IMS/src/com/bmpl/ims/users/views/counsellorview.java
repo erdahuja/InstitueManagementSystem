ddd4901b7a604548efa120355368c99d1cf4df6a
Sunil <vishkarmasunil@gmail.com>
2016-08-11 17:51:40 +0530

    CounsellorView Uploaded

--- IMS/src/com/bmpl/ims/users/views/counsellorview.java
+++ IMS/src/com/bmpl/ims/users/views/counsellorview.java
@@ -3,7 +3,10 @@ package com.bmpl.counsellorform.view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,14 +16,19 @@ import javax.swing.JTextField;
 
 import com.bmpl.counsellorform.DAO.counsellorDAO;
 import com.bmpl.counsellorform.DTO.counsellorDTO;
+import javax.swing.JTextArea;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import java.awt.Font;
 
 public class counsellorview extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField nm;
 	private JTextField ad;
-	private JTextField pn;
 	private JTextField qf;
+	private JTextField pn;
+	private String seletedRedioButton;
 
 	/**
 	 * Launch the application.
@@ -42,12 +50,12 @@ public class counsellorview extends JFrame {
 		setContentPane(contentPane);
 		
 		JLabel lblName = new JLabel("NAME :");
-		lblName.setBounds(10, 47, 46, 14);
+		lblName.setBounds(10, 62, 46, 14);
 		contentPane.add(lblName);
 		
 		nm = new JTextField();
 		nm.setToolTipText("ENTER YOUR NAME");
-		nm.setBounds(130, 44, 213, 20);
+		nm.setBounds(130, 59, 213, 20);
 		contentPane.add(nm);
 		nm.setColumns(10);
 		
@@ -61,23 +69,17 @@ public class counsellorview extends JFrame {
 		contentPane.add(ad);
 		ad.setColumns(10);
 		
-		JLabel lblPhonenumber = new JLabel(" PHONENUMBER :");
+		JLabel lblPhonenumber = new JLabel("GENDER :");
 		lblPhonenumber.setBounds(10, 154, 110, 14);
 		contentPane.add(lblPhonenumber);
 		
-		pn = new JTextField();
-		pn.setToolTipText("YOUR PHONENUMBER PLEASE!!");
-		pn.setBounds(130, 151, 213, 20);
-		contentPane.add(pn);
-		pn.setColumns(10);
-		
 		JLabel lblQualification = new JLabel("QUALIFICATION :");
-		lblQualification.setBounds(10, 212, 96, 14);
+		lblQualification.setBounds(10, 196, 96, 14);
 		contentPane.add(lblQualification);
 		
 		qf = new JTextField();
 		qf.setToolTipText("YOUR QUALIFICATION PLEASE!!");
-		qf.setBounds(130, 209, 213, 20);
+		qf.setBounds(130, 193, 213, 20);
 		contentPane.add(qf);
 		qf.setColumns(10);
 		
@@ -97,14 +99,68 @@ public class counsellorview extends JFrame {
 		btnSubmit.setToolTipText("CLICK AFTER FILLING THE FORM");
 		btnSubmit.setBounds(204, 282, 118, 23);
 		contentPane.add(btnSubmit);
+		
+		JLabel lblPhoneNumber = new JLabel("PHONENUMBER :");
+		lblPhoneNumber.setBounds(10, 235, 96, 14);
+		contentPane.add(lblPhoneNumber);
+		
+		pn = new JTextField();
+		pn.setBounds(130, 232, 213, 20);
+		contentPane.add(pn);
+		pn.setColumns(10);
+		
+		JRadioButton rdbtnMale = new JRadioButton("MALE");
+		ButtonGroup btngroup=new ButtonGroup(); 
+		rdbtnMale.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Enumeration<AbstractButton> allRadioButton=btngroup.getElements();
+				while(allRadioButton.hasMoreElements()) 
+				{
+				JRadioButton temp=(JRadioButton)allRadioButton.nextElement();  
+		           if(temp.isSelected())  
+		           {  
+		        	   seletedRedioButton=temp.getText();  
+		           }
+				}
+				}
+		});
+		rdbtnMale.setBounds(130, 145, 109, 23);
+		contentPane.add(rdbtnMale);
+		
+		JRadioButton rdbtnFemale = new JRadioButton("FEMALE");
+		rdbtnFemale.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Enumeration<AbstractButton> allRadioButton=btngroup.getElements();
+				while(allRadioButton.hasMoreElements()) 
+				{
+				JRadioButton temp=(JRadioButton)allRadioButton.nextElement();  
+		           if(temp.isSelected())  
+		           {  
+		        	   seletedRedioButton=temp.getText();
+		           }
+				}
+			}
+		});
+		
+		rdbtnFemale.setBounds(244, 145, 109, 23);
+		contentPane.add(rdbtnFemale);
+		
+		btngroup.add(rdbtnMale);  
+		btngroup.add(rdbtnFemale);
+		
+		JLabel lblRegistrationForm = new JLabel("REGISTRATION FORM");
+		lblRegistrationForm.setFont(new Font("Yu Gothic", Font.BOLD, 13));
+		lblRegistrationForm.setBounds(187, 11, 156, 14);
+		contentPane.add(lblRegistrationForm);
 	}
 	private void dosubmit() throws ClassNotFoundException, SQLException{
 		counsellorDAO counsellorDAO =new counsellorDAO();
 		counsellorDTO counsellorDTO = new counsellorDTO();
 		counsellorDTO.setName(nm.getText());
 		counsellorDTO.setAddress(ad.getText());
-		counsellorDTO.setPhonenumber(pn.getText());
+		counsellorDTO.setGender(seletedRedioButton);
 		counsellorDTO.setQualification(qf.getText());
+		counsellorDTO.setPhonenumber(pn.getText());
 		boolean isFound=counsellorDAO.counsellor(counsellorDTO);
 		if(isFound)
 		{
