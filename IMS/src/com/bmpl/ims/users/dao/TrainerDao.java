e381354a231c1d26cc6670775bb79e9a7cec6ad8
Himanshu sharma <himanshu648594@gmail.com>
2016-08-11 14:15:04 +0530

    Update TrainerDao.java

--- IMS/src/com/bmpl/ims/users/dao/TrainerDao.java
+++ IMS/src/com/bmpl/ims/users/dao/TrainerDao.java
@@ -1,8 +1,8 @@
 package com.bmpl.ims.users.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.bmpl.ims.common.dao.CommonDAO;
@@ -17,15 +17,17 @@ public interface TrainerDao {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		con = CommonDAO.getConnection();
+		
 		pstmt = con.prepareStatement(sql);
-		pstmt.setString(1, trainer.getTrainer_name());
-		pstmt.setInt(2, trainer.getTrainer_age());
-		pstmt.setString(3, trainer.getTrainer_gender());
-		pstmt.setString(4, trainer.getTrainer_mobileno());
-		pstmt.setString(5, trainer.getTrainer_address());
-		pstmt.setString(6, trainer.getTrainer_email());
-		pstmt.setString(7, trainer.getTrainer_subject());
-		pstmt.setString(8, trainer.getTrainer_image());
+		pstmt.setString(1, trainer.getTrainer_Name());
+		pstmt.setInt(2, trainer.getTrainer_Age());
+		pstmt.setString(3, trainer.getTrainer_Gender());
+		pstmt.setString(4, trainer.getTrainer_Mobileno());
+		pstmt.setString(5, trainer.getTrainer_Address());
+		pstmt.setString(6, trainer.getTrainer_Email());
+		pstmt.setString(7, trainer.getTrainer_Subject());
+		pstmt.setString(8, trainer.getTrainer_Image());
+		pstmt.setDate(9, (Date)trainer.getTrainer_Hire_Date());
 		int result = pstmt.executeUpdate();
 		
 		if(result > 0 ){
@@ -41,6 +43,5 @@ public interface TrainerDao {
 		
 		
 	}
-	
 	
 }
