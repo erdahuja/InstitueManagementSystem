72161a373dd0c9089b451b5d0e543e4ff1c63644
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2016-08-19 10:54:38 +0530

    Update sql.sql

--- IMS/src/sql.sql
+++ IMS/src/sql.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `register_page` (
-  
   `Name` varchar(50) NOT NULL,
   `Address` varchar(50) NOT NULL,
-  `Country` varchar(50) NOT NULL,
+  `Batch` varchar(50) NOT NULL,
   `State` varchar(50) NOT NULL,
   `Course` varchar(100) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Phone` varchar(50) NOT NULL,
-  `Fname` varchar(50) NOT NULL
+  `Fname` varchar(50) NOT NULL,
+  `image` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
